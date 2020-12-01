7f0b209f729d74eed070f50be7557ef9603ce80d
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-01 18:13:15 +0900

    method-overriding

--- src/com/javaex/ex02/ShapeApp.java
+++ src/com/javaex/ex02/ShapeApp.java
@@ -4,18 +4,22 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 		
+		
 		Shape s1 = new Shape("파랑", "파랑");
 		System.out.println("================");
+		//결과 : Shape(2)
 		
 		Triangle t1 = new Triangle();
 		System.out.println("================");
+		//결과 : Shape(0) Triangle(0) --> !주의! 부모클래스의 기본생성자가 기본적으로 실행됨.
 		
 		Triangle t2 = new Triangle(3, 4);
 		System.out.println("================");
+		//결과 : !Shape(2)! Triangle(2) --> super("검정","검정") 이거 파라미터에 부모 필드 없어도 쓸 수 있고, 부모클래스 중 파라미터 2개인 생성자 한번 돌고옴. 
 		
 		Shape t3 = new Triangle("빨강", "빨강", 15, 15);
 		System.out.println("================");
-		
+		//결과 : Shape(2) Triangle(4) 
 	}
 
 }
--- src/com/javaex/ex02/Triangle.java
+++ src/com/javaex/ex02/Triangle.java
@@ -2,7 +2,26 @@ package com.javaex.ex02;
 
 public class Triangle extends Shape{
 	
+	//필드
+	private int width;
+	private int height;
 	
+	//생성자
+	public Triangle() {
+		System.out.println("생성자 Triangle(0) 실행");
+	}
+	public Triangle(int width, int height) {
+		super("검정","검정"); //파라미터에 부모 필드가 없어도 쓸 수 있고 부모클래스 중 파라미터 2개인 생성자 한번 돌고옴.
+		this.width=width;
+		this.height=height;
+		System.out.println("생성자 Triangle(2) 실행");
+	}
+	public Triangle(String fillColor, String lineColor, int Width, int height) {
+		super("검정","검정");
+		this.width=width;
+		this.height=height;
+		System.out.println("생성자 Triangle(4) 실행");
+	}
 }
 
 
