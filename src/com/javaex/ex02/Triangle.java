e2be08d615f939fa4f6879f5778592573b9261b3
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-02 09:42:19 +0900

    method-overriding

--- src/com/javaex/ex02/Triangle.java
+++ src/com/javaex/ex02/Triangle.java
@@ -11,7 +11,7 @@ public class Triangle extends Shape{
 		System.out.println("생성자 Triangle(0) 실행");
 	}
 	public Triangle(int width, int height) {
-		super("검정","검정"); //파라미터에 부모 필드가 없어도 쓸 수 있고 부모클래스 중 파라미터 2개인 생성자 한번 돌고옴.
+		super("검정","검정"); //파라미터에 부모 필드가 없어도 쓸 수 있고 부모클래스 중 파라미터 2개인 생성자 한번 돌고옴. 그냥 있으면 적용되는듯.
 		this.width=width;
 		this.height=height;
 		System.out.println("생성자 Triangle(2) 실행");
--- src/com/javaex/ex03/Shape.java
+++ src/com/javaex/ex03/Shape.java
@@ -2,7 +2,29 @@ package com.javaex.ex03;
 
 public class Shape {
 	
+	//필드
+	/* 자식클래스에서 필드 변수를 갖다 쓸 수 있도록 접근제한자를 변경함.
+	private String fillColor;
+	private String lineColor;
+	*/
+	protected String fillColor;
+	protected String lineColor;
 	
+	//생성자
+	//부모클래스의 기본생성자를 추가해줌.
+	public Shape() {}
+	public Shape(String fillColor, String lineColor) {
+		this.fillColor=fillColor;
+		this.lineColor=lineColor;
+	}
+	
+	//메소드 겟셋
+	public void setFillColor(String fillColor) {
+		this.fillColor=fillColor;
+	}
+	public void setLineColor(String lineColor) {
+		this.lineColor=lineColor;
+	}
 	
 }
 
