9bb0dd770637b63d4ff255e8136714cec3b62f89
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-02 10:41:05 +0900

    method-overriding

--- src/com/javaex/ex05/Depart.java
+++ src/com/javaex/ex05/Depart.java
@@ -1,7 +1,32 @@
 package com.javaex.ex05;
 
-public class Depart {
+public class Depart extends Employee {
 
 	 //코드작성
+	
+	//필드
+	private String department;
+	
+	//생성자
+	public Depart(String name, int salary, String department) {
+		super(name,salary);
+		this.department=department;
+	}
+
+	//메소드 겟셋
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	//메소드 일반 --> 메소드 오버라이딩 시킴.
+	 public void showInformation() {
+	        System.out.println("이름:" + super.getName() + "  연봉:" + super.getSalary()+"  부서:"+department);
+	    }
+	
+	
 
 }
