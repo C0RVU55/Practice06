7a9723f31b78a7be0439962748c140f283b2ecd5
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-03 15:30:56 +0900

    method-overriding

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -5,12 +5,28 @@ public class SmartPhone extends MusicPhone {
     public void execute(String str){
         
         //코드작성
+    	if(str.equals("음악")) {
+    		playMusic();
+    	}else if(str.equals("앱")) {
+    		runApp();
+    	}else {
+    		super.execute(str);
+    		/*
+    		call();이 안 먹어서 super.super.call(); 이런 것도 해봤는데 
+    		당연히 안 되고 MusicPhone에 있는 super.execute(str)타고 Phone의 call();까지 가는 거였음.
+    		*/
+    	}
         
     }
  
     //메소드작성
-    
+    public void runApp() {
+    	System.out.println("앱실행");
+    }
     //메소드작성
+    public void playMusic() {
+    	System.out.println("다운로드해서 음악재생");
+    }
     
     
     
